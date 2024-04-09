ea89406a7fd6666be792beb7559e0611a8aed985
Dzhos_ds17 <Dzhos_ds17@74-16>
2024-04-09 11:27:27 +0300

    new

--- hw_uml8/hw_uml8.cpp
+++ hw_uml8/hw_uml8.cpp
@@ -20,31 +20,43 @@ public:
 	string producer;
 	string type;
 	virtual void Print() abstract;
+	virtual void setName() abstract;
+	virtual void setPrice() abstract;
+	virtual void setProducer() abstract;
+	virtual void setType() abstract;
+
 };
 
 
 
 class RAM : public Device //конкретный реализатор
 {
-	int volume;
 public:
 
-	RAM() {
+	/*void setName() override{}
+	void setPrice() override {}
+	void setProducer() override {}
+	void setType() override {}*/
+
+	void setName() override {
 		name = "Corsair Vengeance LPX";
-		price = 100;
-		producer = "Corsair";
-		type = "DDR4";
-		volume = 8096;
-
-
 	}
+	void setPrice() override {
+		price = 100;
+	}
+	void setProducer() override {
+		producer = "Corsair";
+	}
+	void setType() override {
+		type = "DDR4";
+	}
+
 
 	void Print() override {
 		cout << "name: " << name << endl;
 		cout << "price: " << price << endl;
 		cout << "producer: " << producer << endl;
 		cout << "type: " << type << endl;
-		cout << "volume: " << volume << endl;
 	}
 
 	
@@ -53,13 +65,20 @@ public:
 
 class HDD : public Device { //конкретный реализатор
 public:
-	HDD() {
-		name = "Seagate Barracuda";
-		price = 80;
-		producer = "Seagate";
-		type = "SATA 3.5";
 
+	void setName() override {
+		name = "Seagate Barracuda";
 	}
+	void setPrice() override {
+		price = 80;
+	}
+	void setProducer() override {
+		producer = "Seagate";
+	}
+	void setType() override {
+		type = "SATA 3.5";
+	}
+
 
 	void Print() override {
 		cout << "name: " << name << endl;
@@ -71,13 +90,21 @@ public:
 
 class VGA : public Device { //конкретный реализатор
 public:
-	VGA() {
-		name = "Sapphire Radeon RX 6700 XT Nitro+";
-		price = 500;
-		producer = "Sapphire";
-		type = "PCIe 4.0";
 
+
+	void setName() override {
+		name = "Sapphire Radeon RX 6700 XT Nitro+";
 	}
+	void setPrice() override {
+		price = 500;
+	}
+	void setProducer() override {
+		producer = "Sapphire";
+	}
+	void setType() override {
+		type = "PCIe 4.0";
+	}
+
 
 	void Print() override {
 		cout << "name: " << name << endl;
@@ -88,7 +115,7 @@ public:
 };
 
 
-class iPrint abstract //абстракция
+class iPrint //абстракция
 {
 protected:
 	Device* device;
@@ -100,19 +127,20 @@ public:
 	void setDevice(Device* device)
 	{
 		this->device = device;
+		device->setName();
+		device->setPrice();
+		device->setProducer();
+		device->setType();
 	}
 
-	virtual void PrintInfo() abstract;
+		void PrintInfo() {
+		device->Print();
+	}
 	
 
 };
 
-class Print : public iPrint {
-public:
-	void PrintInfo() override{
-		device->Print();
-	}
-};
+
 
 void client(iPrint* print, Device* device) //клиент
 {
@@ -122,9 +150,20 @@ void client(iPrint* print, Device* device) //клиент
 int main()
 {
 	Device* device = new RAM();
-	iPrint* print = new Print();
+	iPrint* print = new iPrint();
 	client(print, device);
 	print->PrintInfo();
+	cout << endl;
+
+	device = new HDD();
+	client(print, device);
+	print->PrintInfo();
+	cout << endl;
+
+	device = new VGA();
+	client(print, device);
+	print->PrintInfo();
+	delete device;
 	delete print;
 }
 
